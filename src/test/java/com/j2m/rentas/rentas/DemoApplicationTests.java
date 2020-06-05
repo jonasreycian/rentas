2e3ce0c0218fed89b350d88739028d37bd03e6a1
Jonas Reycian <cj.saraosos@gmail.com>
2020-06-06 00:16:57 +0800

    Initial commit

---
+++ src/test/java/com/j2m/rentas/rentas/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.j2m.rentas.rentas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
